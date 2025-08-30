0ca1894292f45d6654ec43f72d1549281b3757ef
Abdullah <abd027282930@gmail.com>
2025-08-30 23:13:49 +0300

    first commit

---
+++ src/min.js
@@ -0,0 +1,13 @@
+function scrollWin() {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  });
+}
+function cilc() {
+ location.href = "https://abdullah135792.github.io/The-tenth-project/";
+}
+function cil() {
+ location.href = "https://github.com/Abdullah135792/The-tenth-project/blob/main/index.html  ";
+}
